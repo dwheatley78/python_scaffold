aa3669e5d9bf43cca11b5fdc9ce6db04ef216016
EC2 Default User <ec2-user@ip-172-31-28-168.us-east-2.compute.internal>
2021-10-20 13:13:17 +0000

    adding initial structure

---
+++ hello.py
@@ -0,0 +1,12 @@
+def add(x, y):
+    return x + y
+
+
+def main():
+    x = 2#int(input("Type a value for x: "))
+    y = 3#int(input("Type a value for y: "))
+    result = add(x, y)
+    print(f"This is the sum of {x} + {y} = {result}")
+
+
+main()
